810cc889c7dec7033992bdd2d26e78047f4d9a17
Alex Grover <hello@alexgrover.me>
2022-10-15 21:20:40 -0700

    Add SEO

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,9 @@
 export const NAME = 'Alex Grover'
+export const SEPARATOR = '|'
+export const TAGLINE = 'Create > Consume'
+export const DESCRIPTION = `${NAME}'s personal website.`
 export const EMAIL = 'hello@alexgrover.me'
 export const GITHUB_USERNAME = 'alex-grover'
 export const INSTAGRAM_USERNAME = 'grovemau5'
 export const LINKEDIN_USERNAME = 'alex-grover'
-export const TWITTER_USERNAME = '@grovemau5'
+export const TWITTER_USERNAME = 'grovemau5'
