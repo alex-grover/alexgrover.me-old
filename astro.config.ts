9f888fbdef4e8d3428f7e9fb29c0b1ec7a224d41
Alex Grover <hello@alexgrover.me>
2022-11-03 19:53:16 -0700

    Add sitemap

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,11 @@
 import compress from 'astro-compress'
 import { defineConfig } from 'astro/config'
 import { SITE } from './src/constants'
+import sitemap from '@astrojs/sitemap'
 
 export default defineConfig({
   site: SITE,
-  integrations: [compress()],
+  integrations: [compress(), sitemap()],
   vite: {
     server: {
       watch: {
