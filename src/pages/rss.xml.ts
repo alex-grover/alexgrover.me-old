e93db8e358629e86361fedb4d85e2de38eeeac88
Alex Grover <hello@alexgrover.me>
2023-01-30 13:41:14 -0800

    Update RSS configuration for Astro 2

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,12 +1,12 @@
-import rss from '@astrojs/rss'
+import rss, { pagesGlobToRssItems } from '@astrojs/rss'
 import { DESCRIPTION, NAME, SEPARATOR, TAGLINE } from '../constants'
 
-export function get() {
+export async function get() {
   return rss({
     title: `${NAME}${SEPARATOR}${TAGLINE}`,
     description: DESCRIPTION,
     site: import.meta.env.SITE,
-    items: import.meta.glob('./posts/*.md'),
+    items: await pagesGlobToRssItems(import.meta.glob('./posts/*.md')),
     stylesheet: '/pretty-feed.xsl',
   })
 }
