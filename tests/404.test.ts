ee5437751c1837eb6df14d5b29acec77b095c3c3
Alex Grover <hello@alexgrover.me>
2023-02-03 09:18:36 -0800

    Add more tests

---
+++ tests/404.test.ts
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test'
+
+test('not found page loads', async ({ page }) => {
+  await page.goto('/not-found')
+  await expect(page).toHaveTitle('Alex Grover | Not Found')
+  await expect(page.getByRole('heading', { name: '404' })).toBeVisible()
+  const link = page.getByTestId('link')
+  await expect(link).toBeVisible()
+  await expect(link).toHaveText('Go home')
+  await expect(link).toHaveAttribute('href', '/')
+})
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -3,6 +3,7 @@ import { test, expect } from '@playwright/test'
 test('homepage loads', async ({ page }) => {
   await page.goto('/')
   await expect(page).toHaveTitle('Alex Grover | Create > Consume')
-  const brand = page.getByText('Alex Grover')
+  const brand = page.getByRole('link', { name: 'Alex Grover' })
+  await expect(brand).toBeVisible()
   await expect(brand).toHaveAttribute('href', '/')
 })
