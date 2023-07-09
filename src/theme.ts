d61ac0e4ac88c207d6055befe5bff9058321d351
Alex Grover <hello@alexgrover.me>
2023-07-09 13:51:38 +0200

    Update theme-color meta tag with theme

---
+++ src/theme.ts
@@ -0,0 +1,8 @@
+export function updateThemeColor() {
+  const backgroundColor = getComputedStyle(
+    document.documentElement
+  ).getPropertyValue('--c-bg')
+  document
+    .querySelector('meta[name=theme-color]')
+    ?.setAttribute('content', backgroundColor)
+}
