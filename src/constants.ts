dadec2b2cea14da6b5be1c7f206174574097f941
Alex Grover <hello@alexgrover.me>
2023-07-20 22:25:30 +0200

    Update instagram username

--- src/constants.ts
+++ src/constants.ts
@@ -5,13 +5,11 @@ export const TAGLINE = 'Create > Consume'
 export const DESCRIPTION = `${NAME}'s personal website.`
 
 export const EMAIL = 'hello@alexgrover.me'
-export const FOUNDATION_USERNAME = '@alex-grover'
 export const GITHUB_USERNAME = 'alex-grover'
-export const INSTAGRAM_USERNAME = 'alexgrover_eth'
+export const INSTAGRAM_USERNAME = 'alex__grover'
 export const READ_CV_USERNAME = 'alexgrover'
 export const TWITTER_USERNAME = 'alexgrover_eth'
 
-export const FOUNDATION_LINK = `https://foundation.app/${FOUNDATION_USERNAME}`
 export const GITHUB_LINK = `https://github.com/${GITHUB_USERNAME}`
 export const INSTAGRAM_LINK = `https://www.instagram.com/${INSTAGRAM_USERNAME}/`
 export const TWITTER_LINK = `https://twitter.com/${TWITTER_USERNAME}`
